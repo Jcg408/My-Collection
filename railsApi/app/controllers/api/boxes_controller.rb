114e92075c925451dc9b526212df096fdfa4be13
Jessica Gladstone <jessgladtobe@hotmail.com>
2019-06-20 22:27:12 -0400

    add boxes controller, routes

---
+++ railsApi/app/controllers/api/boxes_controller.rb
@@ -0,0 +1,47 @@
+class Api::BoxesController < ApplicationController
+    before_action :set_box, only: [:show, :update, :destroy]
+    
+    def index
+        render :json => Box.all
+    end
+
+    def create
+        box = Box.new(box_params)
+        if box.save
+            render :json => box
+        else
+            render :json => {message: box.errors}, status: 400
+        end
+
+    end
+
+    def show
+        render :json => box
+    end
+
+    def update
+       if box.update(box_params)
+        render :json => box 
+       else
+        render :json => {message: box.errors}, status: 400
+       end
+    end
+
+    def destroy
+        if box.destroy
+            render :json => {message: "box has been deleted"}, status: 204
+        else
+            render :json => {message: "box Not Deleted"}, status: 400
+        end
+    end
+
+    private
+
+    def set_box
+        box = Box.find_by(id: params[:id])
+    end
+
+    def box_params
+        params.require(:box).permit(:name, :description, :year, :imgUrl )
+    end
+end
--- railsApi/config/routes.rb
+++ railsApi/config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  
+  namespace :api do
+    resources :boxes, except: [:edit]
+  end
 end
