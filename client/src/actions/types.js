c734c2b2b9e713ee1f3312fdbbbc0211a809999e
Jessica Gladstone <jessgladtobe@hotmail.com>
2019-06-20 23:08:43 -0400

    add actions, reducers and store

---
+++ client/src/actions/types.js
@@ -0,0 +1,2 @@
+export const FETCH_BOXES = 'FETCH_BOXES';
+export const NEW_BOX = 'NEW_BOX';
---
+++ client/src/reducers/boxReducer.js
@@ -0,0 +1,23 @@
+import {FETCH_BOXES, NEW_BOX} from '../actions/types';
+
+const initialState = {
+    items: [],
+    item: {}
+}
+export default function (state = initialState, action) {
+    switch (action.type) {
+       case FETCH_BOXES:
+           return {
+               ...state,
+               items: action.payload
+           }
+
+        case NEW_BOX:
+           return {
+               ...state,
+               iItem: action.payload
+           }
+        default:
+            return state;
+    }
+}
