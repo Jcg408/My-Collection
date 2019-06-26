3e13c29f8c546b4bc847f964ec2ce77fba548fdb
Jessica Gladstone <jessgladtobe@hotmail.com>
2019-06-26 11:57:56 -0400

    add reducers to combinereducers

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,8 +1,10 @@
 import {combineReducers} from 'redux';
 import boxReducer from './boxReducer';
+import noteReducer from './noteReducer';
 
 const rootReducer =  combineReducers({
     boxes: boxReducer
+    notes: noteReducer
 });
 
 export default rootReducer;
