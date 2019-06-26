df55653c95d145364f9cd3af325033890876e1f7
Jessica Gladstone <jessgladtobe@hotmail.com>
2019-06-26 12:21:51 -0400

    create note model and table

--- railsApi/db/schema.rb
+++ railsApi/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_20_193010) do
+ActiveRecord::Schema.define(version: 2019_06_26_161641) do
 
   create_table "boxes", force: :cascade do |t|
     t.string "name"
@@ -20,6 +20,13 @@ ActiveRecord::Schema.define(version: 2019_06_20_193010) do
     t.string "imgUrl"
     t.string "artist"
     t.string "location"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "notes", force: :cascade do |t|
+    t.string "subject"
+    t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
